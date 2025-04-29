4e5fd749c20e458b87d3901579a47787ec4df6f4
tugas45 <racerftail@gmail.com>
2025-04-29 23:54:23 +0700

    update foto

--- lib/Tugas_profile_with_asset/profile_asset.dart
+++ lib/Tugas_profile_with_asset/profile_asset.dart
@@ -1,49 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ProfileDetail extends StatelessWidget {
-  const ProfileDetail({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("Profil IBNU"),
+      appBar: AppBar(title: Text('Profil')),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('assets/pp.jpg'),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'IBNU MUH MUKHLIS',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: const Center(child: ProfilePhoto()),
-    );
-  }
-}
-
-class ProfilePhoto extends StatelessWidget {
-  const ProfilePhoto({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomRight,
-      children: [
-        // Gambar Profil
-        const CircleAvatar(
-          radius: 80,
-          backgroundImage: AssetImage('assets/me.jpg'),
-        ),
-        // Ikon Edit
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
-          child: IconButton(
-            icon: const Icon(Icons.edit, color: Colors.white),
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Edit Profile diklik')),
-              );
-            },
-          ),
-        ),
-      ],
     );
   }
 }
