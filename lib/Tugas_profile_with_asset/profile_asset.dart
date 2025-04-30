efb71c50f0c25161dd35c9f5f3d3b62018b159cb
tugas45 <racerftail@gmail.com>
2025-04-30 09:18:36 +0700

    up054

--- lib/Tugas_profile_with_asset/profile_asset.dart
+++ lib/Tugas_profile_with_asset/profile_asset.dart
@@ -4,34 +4,36 @@ class ProfileDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profil')),
+      appBar: AppBar(
+        title: const Text(
+          'Profile',
+          style: TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 197, 39),
+      ),
+      backgroundColor: Color.fromARGB(255, 86, 179, 255),
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/img/pp.jpg'),
-                ),
-                const SizedBox(height: 30),
-                const Text(
-                  'Ibnu Muh Mukhlis',
-                  style: TextStyle(fontSize: 18, fontFamily: 'Raphtalia'),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  '220102037',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'TRPL 22A2',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-              ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('assets/img/pp.jpg'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Ibnu Muh Mukhlis',
+              style: TextStyle(fontSize: 30, fontFamily: 'Raphtalia'),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '220102037',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'TRPL 22A2',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
             ),
           ],
         ),
